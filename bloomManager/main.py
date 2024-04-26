bbe0a3daadbaba74b8016bf9ee93efa33c84b8a1
windfall0369 <tlsehdrl4340@naver.com>
2024-04-26 11:38:09 +0900

    feat: setup bloomManager

---
+++ bloomManager/main.py
@@ -0,0 +1,20 @@
+import sys
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+
+
+
+name = '005930.KS'
+ticker = yf.Ticker(name)
+df = ticker.history(interval='1d', period='1y', auto_adjust=False)
+
+print(df.tail())
+print(df.info())
+
+
+
+
